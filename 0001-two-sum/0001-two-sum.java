fe830878b872e9850126022ba384759e1145eebb
vedant purohit <143059971+VedPurohit25@users.noreply.github.com>
2026-03-01 20:35:03 +0530

    Time: 9 ms (46.44%), Space: 46.7 MB (93.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,34 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Outer loop picks the first number
-        for (int i = 0; i < nums.length; i++) {
-            // Inner loop checks every number AFTER 'i'
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+        int n = nums.length;
+        int[][] pair = new int[n][2];
+        
+        // Store value and original index
+        for (int i = 0; i < n; i++) {
+            pair[i][0] = nums[i];
+            pair[i][1] = i;
+        }
+        
+        // Sort by value
+        Arrays.sort(pair, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        int left = 0;
+        int right = n - 1;
+        
+        while (left < right) {
+            int sum = pair[left][0] + pair[right][0];
+            
+            if (sum == target) {
+                return new int[] { pair[left][1], pair[right][1] };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return new int[] {}; // No solution found
+        
+        return new int[] {};
     }
 }
