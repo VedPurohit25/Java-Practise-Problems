3e9fec81d083e07acef790bc15cb4cb731305397
vedant purohit <143059971+VedPurohit25@users.noreply.github.com>
2026-03-01 20:51:28 +0530

    Time: 1570 ms (5.21%), Space: 48.3 MB (98.08%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,25 +1,19 @@
-import java.util.HashMap;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
-        int currentSum = 0;
-        // Map to store: <PrefixSum, Number of times it occurred>
-        HashMap<Integer, Integer> map = new HashMap<>();
         
-        // Base case: a prefix sum of 0 has occurred once
-        map.put(0, 1);
-        
-        for (int num : nums) {
-            currentSum += num;
+        // Try every possible starting position
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
             
-            // If (currentSum - k) exists, it means a subarray sum of k exists
-            if (map.containsKey(currentSum - k)) {
-                count += map.get(currentSum - k);
+            // For each start, try every possible ending position
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                
+                if (currentSum == k) {
+                    count++;
+                }
             }
-            
-            // Record this currentSum in the map
-            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
         }
         
         return count;
