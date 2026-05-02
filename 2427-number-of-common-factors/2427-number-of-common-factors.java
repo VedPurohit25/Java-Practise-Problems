badd3bb8a724ca1e43a1e97702e979cb918492cc
vedant purohit <143059971+VedPurohit25@users.noreply.github.com>
2026-05-02 19:32:40 +0530

    Time: 1 ms (61.7%), Space: 42.2 MB (40.85%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -2,7 +2,7 @@ class Solution {
     public int commonFactors(int a, int b) {
         int count = 0;
         // The common factor cannot exceed the smaller number
-        int limit = Math.min(a, b);
+        int limit = (a < b) ? a : b;
         
         for (int i = 1; i <= limit; i++) {
             // Check if i divides both a and b perfectly
