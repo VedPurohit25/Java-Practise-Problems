03661b58218ca8bd7ec32be8544c9820244b84f4
vedant purohit <143059971+VedPurohit25@users.noreply.github.com>
2026-07-06 15:07:34 +0530

    Time: 1 ms (77.53%), Space: 42.7 MB (77.09%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        // Split strings by literal dot (requires escaping because split takes regex)
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        
+        int maxLength = Math.max(v1.length, v2.length);
+        
+        for (int i = 0; i < maxLength; i++) {
+            // If we run out of revisions for a version, default its value to 0
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            
+            if (num1 < num2) {
+                return -1;
+            } else if (num1 > num2) {
+                return 1;
+            }
+        }
+        
+        // If all compared components are equal
+        return 0;
+    }
+}
